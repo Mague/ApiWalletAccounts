374df6939e228857160378d7102f647ab5309c6a
Mague <enmanueldavidmolina@gmail.com>
2017-06-05 23:30:57 -0400

    Refactorizacion de la funcionalidad de conexion a la base de datos ya que era muy repetitiva falta modificar los demas archivos donde se utiliza

--- api/user.go
+++ api/user.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/Mague/ApiWalletAccounts/models"
+	"github.com/Mague/ApiWalletAccounts/utils"
 	"github.com/asdine/storm"
 	"github.com/gin-gonic/gin"
 )
@@ -27,63 +28,49 @@ func (this User) Load(engine *gin.Engine) {
 	}
 }
 func (this User) get(ctx *gin.Context) {
-	db, err := storm.Open("wallet.db")
-	if err != nil {
-		fmt.Println("error al abrir la base de datos")
-	} else {
-		fmt.Println("Conexion exitosa")
-	}
 	var rUser models.User
-	if userId, err := strconv.Atoi(ctx.Param("id")); err == nil {
+	utils.Query(func(db *storm.DB) {
+		if userId, err := strconv.Atoi(ctx.Param("id")); err == nil {
 
-		fmt.Println(&rUser)
+			fmt.Println(&rUser)
 
-		err = db.One("ID", userId, &rUser)
-		if err != nil {
-			fmt.Println(err)
+			err = db.One("ID", userId, &rUser)
+			if err != nil {
+				fmt.Println(err)
+			}
 		}
-	}
-
-	db.Close()
+	})
 	ctx.JSON(http.StatusOK, &rUser)
 }
 func (this User) all(ctx *gin.Context) {
-	db, err := storm.Open("wallet.db")
-	if err != nil {
-		fmt.Println("Error al abrir la base de datos")
-	} else {
-		fmt.Println("Conexion exitosa")
-	}
 	var rUsers []models.User
-
+	utils.Query(func(db *storm.DB) {
+		err := db.AllByIndex("ID", &rUsers, storm.Reverse())
+		if err != nil {
+			fmt.Println("Error al obtener las cuentas")
+		} else {
+			fmt.Println(&rUsers)
+		}
+	})
 	// err = db.Select(q.Eq("UserName", "mague")).Find(&rUsers)
-	err = db.AllByIndex("ID", &rUsers, storm.Reverse())
-	if err != nil {
-		fmt.Println("Error al obtener las cuentas")
-	} else {
-		fmt.Println(&rUsers)
-	}
-	db.Close()
+
 	ctx.JSON(http.StatusOK, &rUsers)
 }
 
 func (this User) add(ctx *gin.Context) {
 	fmt.Println("accounts/add")
-	db, err := storm.Open("wallet.db")
-	if err != nil {
-		fmt.Println("Error al abrir la base de datos")
-	} else {
-		fmt.Println("Conexion exitosa")
-	}
-	data := models.User{
-		UserName:  ctx.PostForm("userName"),
-		Email:     ctx.PostForm("email"),
-		Password:  ctx.PostForm("pwd"),
-		CreatedAt: time.Now(),
-	}
-	err = db.Save(&data)
-	fmt.Println(err)
-	db.Close()
+	var err error
+	utils.Query(func(db *storm.DB) {
+		data := models.User{
+			UserName:  ctx.PostForm("userName"),
+			Email:     ctx.PostForm("email"),
+			Password:  ctx.PostForm("pwd"),
+			CreatedAt: time.Now(),
+		}
+		err = db.Save(&data)
+		fmt.Println(err)
+	})
+
 	if err != nil {
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "Error al añadir al usuario",
@@ -93,5 +80,4 @@ func (this User) add(ctx *gin.Context) {
 			"message": "Usuario añadido correctamente",
 		})
 	}
-
 }
